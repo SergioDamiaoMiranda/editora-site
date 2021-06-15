09734cb316b09e2d91ab52ffe87da6bf220c0865
SergioDamiaoMiranda <48809853+SergioDamiaoMiranda@users.noreply.github.com>
2021-06-14 21:13:52 -0300

    Cap 4 e 5 final

---
+++ src/components/Logo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const logo = () => {
+    return (
+        <a href="#">
+            <h1 className="logo">
+                <span>Editora MaujorTec</span>
+            </h1>
+        </a>
+    );
+};
+
+export default logo;
---
+++ src/components/Rodape.js
@@ -0,0 +1,11 @@
+import React    from "react";
+
+const Rodape = () => {
+    return (
+        <footer className="rodape">
+            Conteúdo cedido pela Editora Novatec &#8212; &copy; Copyright 2021.
+        </footer>
+    );
+};
+
+export default Rodape;
