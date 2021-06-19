fd255acf1977328e2815c64cf6c8190d9a07c0c9
SergioDamiaoMiranda <48809853+SergioDamiaoMiranda@users.noreply.github.com>
2021-06-19 16:56:28 -0300

    Fim cap 7

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { Component }      from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Topo       from "./components/Topo";
-import Home       from "./components/Home";
-import Catalogo   from "./components/Catalogo";
-import Frontend   from "./components/Frontend";
-import NotFound   from "./components/NotFound";
-import Rodape     from "./components/Rodape";
+import Topo         from "./components/Topo";
+import Home         from "./components/Home";
+import Catalogo     from "./components/Catalogo";
+import Frontend     from "./components/Frontend";
+import NotFound     from "./components/NotFound";
+import Rodape       from "./components/Rodape";
+import Programacao  from "./components/Programacao";
+import Desing       from "./components/Desing";
+import Livro        from "./components/Livro";
+
+import axios        from "axios";
 
 import "./index.css"
-import Programacao from "./components/Programacao";
-import Desing from "./components/Desing";
+
 
 class App extends Component {
+  //Criando o array livros vazio
+  state = {
+    livros: []
+  };
+  async componentDidMount() {
+    try {
+      const { data: livros } = await axios.get("/api/todosOsLivros.json");
+      this.setState({ livros });
+    } catch (error) {
+      console.log(error);
+      document
+        .querySelectorAll(".principal")[0].insertAdjacentHTML("beforeend", "<p class='erro'>Msg de erro</p>")
+    }
+  }
   render() {
     return (
       <Router>
